5d2a2e60ea346761602cae7aa8a4933c87f57dea
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2026-03-27 03:13:20 -0700

    Implement prependMessage, submitMessage, and loadMessages

--- guestbook/script.js
+++ guestbook/script.js
@@ -7,39 +7,103 @@ const ta = new TimeAgo('en-US');
 
 console.log(ta.format(new Date(1774536303551)));
 
-const nameBox    = document.getElementById('name-box');
-const messageBox = document.getElementById('message-box');
-const sendButton = document.getElementById('send-button')
-const messages   = document.getElementById('messages')
+const inputBox      = document.getElementById('input-box');
+const nameBox       = document.getElementById('name-box');
+const messageBox    = document.getElementById('message-box');
+const sendButton    = document.getElementById('send-button')
+const refreshButton = document.getElementById('refresh-button');
+const messages      = document.getElementById('messages')
 
-function appendPost(name, timestamp, message) {
-    const newPost = document.createElement('div');
-    newPost.className = 'guestbook-post';
-    newPost.innerHTML = `
-        <time datetime="${new Date(timestamp).toISOString()}">${ta.format(timestamp)}</time>
-        <p><strong>From:</strong> ${name}</p>
-        <br>
-        <p>${message}</p>
+function prependMessage(name, created_at, message) {
+    const date = new Date(created_at);
+    const newMessage = document.createElement('div');
+    newMessage.className = 'guestbook-message';
+    newMessage.innerHTML = `
+        <div class="header">
+            <p>From: ${name}</p>
+            <time datetime="${date.toISOString()}" title="${date.toLocaleString()}">${ta.format(created_at)}</time>
+        </div>
+        <div class="body">
+            <p>${message}</p>
+        </div>
     `;
-    messages.appendChild(newPost);
+    messages.prepend(newMessage);
 }
 
-async function makePost() {
-    console.log({name: nameBox.value, created_at: Date.now(), message: messageBox.value});
-    // if (nameBox.value && messageBox.value) {
-    //     const { error } = await supabase
-    //                         .from('guestbook')
-    //                         .insert({
-    //                             id: 1,
-    //                             name: nameBox.value,
-    //                             created_at: Date.now(),
-    //                             message: messageBox.value
-    //                         });
-    //     console.log(error);
-    // }
+async function submitMessage() {
+    let name = nameBox.value?.trim();
+    let message = messageBox.value?.trim();
+
+    if (name?.length > 0 && message?.length > 0) {
+        // send data to supabase
+        const date = Date.now();
+        const {error} = await supabase.from('guestbook').insert({
+            name: name,
+            created_at: date,
+            message: message
+        });
+
+        // handle error
+        if (error) {
+            console.log('submitMessage:', error);
+
+            // display error message
+            const error_message = document.createElement('div');
+            error_message.innerHTML = `
+                <br>
+                <p><strong>ERROR:</strong> Couldn't submit message.</p>
+            `;
+            inputBox.appendChild(error_message);
+            setTimeout(() => error_message.remove(), 3000);
+        }
+        else {
+            console.log('Submitted message');
+            console.log({name: name, created_at: date, message: message});
+
+            // refresh messages
+            loadMessages();
+
+            // clear name and message boxes
+            nameBox.value = '';
+            messageBox.value = '';
+        }
+    }
+    else {
+        console.log('Couldn\'t submit message: one or more fields empty.');
+    }
 }
 
-sendButton.addEventListener('click', makePost);
+async function loadMessages() {
+    // clear existing messages
+    messages.innerHTML = '';
 
-// const { error } = await supabase.from('guestbook').insert({id: 1, name: 'Doduodrio', created_at: Date.now(), message: 'Hello World'});
-// console.log(error);
+    const loading = document.createElement('p');
+    loading.innerText = 'Loading messages...';2
+    messages.appendChild(loading);
+
+    // get data from supabase
+    const {data, error} = await supabase.from('guestbook').select();
+
+    loading.remove();
+    if (error) {
+        console.log('loadMessages:', error);
+
+        // display error message
+        const errorMessage = document.createElement('div');
+        errorMessage.innerHTML = `
+            <p><strong>ERROR:</strong> Couldn't load messages.</p>
+        `;
+        messages.appendChild(errorMessage);
+    }
+    else {
+        console.log('Retrieved data');
+        for (let i=0; i<data.length; i++) {
+            prependMessage(data[i].name, data[i].created_at, data[i].message);
+        }
+        console.log('Loaded messages');
+    }
+}
+
+sendButton.addEventListener('click', submitMessage);
+refreshButton.addEventListener('click', loadMessages);
+loadMessages();
